60516cffd63eb575c73d4f59dec15b0b05ba10c0
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-01-04 11:43:27 +0200

    feat: install react spinners

---
+++ src/component/Loader/Loader.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { PacmanLoader } from 'react-spinners';
+
+export const Loader: FC = () => {
+  return <PacmanLoader color="#36d7b7" />;
+};
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from '../../store/hook/hook';
 import { ILang } from '../../types/localisation';
 import { Header } from '../../component/Header/Header';
 import { EditorComponent } from '../../component/EditorComponent/EditorComponent';
+import { Loader } from '../../component/Loader/Loader';
 
 export const Main: FC = () => {
   const lang = useAppSelector((store) => store.changeLang.language);
@@ -11,7 +12,8 @@ export const Main: FC = () => {
     <>
       <Header />
       {text[lang].main + lang}
-      <Suspense>
+
+      <Suspense fallback={<Loader />}>
         <EditorComponent />
       </Suspense>
     </>
