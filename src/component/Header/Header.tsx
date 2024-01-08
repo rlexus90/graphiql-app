5b6aaedad9d2f96f94d3a3b54dc8eba40deaa8bb
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-01-08 12:48:22 +0200

    feat: add Stiky style to header

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,18 +1,36 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useActions, useAppSelector } from '../../store/hook/hook';
 import { ILang } from '../../types/localisation';
+import clsx from 'clsx';
 import style from './Header.module.scss';
 
 const Header: FC = () => {
   const lang = useAppSelector((store) => store.changeLang.language);
   const { changeLang } = useActions();
   const isLogin = useAppSelector((store) => store.authSlice.isLogin);
+  const [stiky, setStiky] = useState(false);
+
+  const stikyHeader = () => {
+    const currentScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    console.log(currentScroll);
+    if (currentScroll > 20) setStiky(true);
+    if (currentScroll == 0) setStiky(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', stikyHeader);
+
+    return () => window.removeEventListener('scroll', stikyHeader);
+  }, []);
 
   return (
     <>
-      <header>
-        <div className={style.wrapper}>
+      <header className={clsx({ [style.header_stiky]: stiky })}>
+        <div
+          className={clsx({ [style.wrapper]: !stiky, [style.stiky]: stiky })}
+        >
           <nav className={style.nav}>
             <ul>
               <li>
