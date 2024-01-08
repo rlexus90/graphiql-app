3edde61d7abbba2eef046afe476365ff6bfb5033
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-01-08 09:53:01 +0200

    feat: add About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,15 +1,38 @@
 import { FC } from 'react';
 import Header from '../../component/Header/Header';
 import Footer from '../../component/Footer/Footer';
+import { useAppSelector } from '../../store/hook/hook';
+import { ILang } from '../../types/localisation';
+import style from './About.module.scss';
 
 const About: FC = () => {
+  const lang = useAppSelector((store) => store.changeLang.language);
+
   return (
     <>
       <Header />
-      About work
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <img className={style.photo} src="/photo.jpg" alt="my Photo" />
+          <p>{text[lang].description}</p>
+        </div>
+      </div>
       <Footer />
     </>
   );
 };
 
 export default About;
+
+const text: ILang = {
+  Ua: {
+    description: `Привіт! Я фронтернт розробник початківець.
+		Відтоді як я почав вивчати фронтенд - я все більше захоплююсь програмуванням і прагну вивчити нові та цікаві для мене технології.\n
+		Цей проєкт створений для роботи з Graphql запитами. `,
+  },
+  En: {
+    description: `Hello! I am a beginner frontend developer.
+		Since I started learning frontend, I am more and more interested in programming and I want to learn new and interesting technologies for me.
+		This project was created to work with Graphql requests.`,
+  },
+};
